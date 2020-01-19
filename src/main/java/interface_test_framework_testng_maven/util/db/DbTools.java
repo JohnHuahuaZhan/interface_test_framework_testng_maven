e493331b82edbf716382ef8a94dd72ff09b2c400
xifan <zhu13486229656@163.com>
2020-01-19 15:06:56 +0800

    update 增加c3p0自动回收链接

--- src/main/java/interface_test_framework_testng_maven/util/db/DbTools.java
+++ src/main/java/interface_test_framework_testng_maven/util/db/DbTools.java
@@ -4,6 +4,7 @@ import interface_test_framework_testng_maven.datasource.DataSourceFactory;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -12,12 +13,16 @@ import java.util.List;
  */
 public class DbTools {
     public static <T> List<T> getData(String sql, String dataBase, Class<? extends T> classEnter, Object... params) throws SQLException {
-        QueryRunner qr = new QueryRunner(DataSourceFactory.getInstance().getDataSource(dataBase));
+        DataSource conn=DataSourceFactory.getInstance().getDataSource(dataBase);
+        QueryRunner qr = new QueryRunner(conn);
+        conn.getConnection().close();
         return qr.query(sql, new BeanListHandler<T>(classEnter), params);
     }
 
     public static int updateData(String sql, String dataBase, Object... params) throws SQLException {
-        QueryRunner qr = new QueryRunner(DataSourceFactory.getInstance().getDataSource(dataBase));
+        DataSource conn=DataSourceFactory.getInstance().getDataSource(dataBase);
+        QueryRunner qr = new QueryRunner(conn);
+        conn.getConnection().close();
         return qr.update(sql, params);
     }
 }
